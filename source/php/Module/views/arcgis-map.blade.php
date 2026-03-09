d354fdb594fde5a17ababa7527d920dbcac37351
William Lundqvist <william.lundqvist@consid.se>
2026-03-09 14:05:21 +0100

    Refactor ArcGIS map view to use semantic HTML with figure and figcaption elements for better accessibility and styling. Added corresponding styles for map caption.

--- source/php/Module/views/arcgis-map.blade.php
+++ source/php/Module/views/arcgis-map.blade.php
@@ -8,14 +8,12 @@
     @endtypography
 @endif
 
-{!! $mapHtml !!}
+<figure class="arcgis-map-figure u-margin__y--0">
+    {!! $mapHtml !!}
 
-@if (!empty($mapDescription))
-    @typography([
-        'element' => 'div',
-        'variant' => 'body',
-        'classList' => ['arcgis-map-description', 'u-margin__top--2']
-    ])
-        {!! $mapDescription !!}
-    @endtypography
-@endif
+    @if (!empty($mapDescription))
+        <figcaption class="arcgis-map-caption u-margin__top--1 u-font-size--meta u-color__text--muted">
+            {!! $mapDescription !!}
+        </figcaption>
+    @endif
+</figure>
